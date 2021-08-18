cc108d2c1bd7bbd837b1f609cfd14753c2f36b02
Yaroslav Homenko <yaroslav.homenko@sdk.finance>
2021-08-18 18:54:24 +0300

    Added exercise solution for counter actor and bank account actor

--- src/main/scala/part2actors/ActorCapabilities.scala
+++ src/main/scala/part2actors/ActorCapabilities.scala
@@ -1,6 +1,9 @@
 package part2actors
 
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
+//import part2actors.ActorCapabilities.BankAccount.{Deposit, Withdraw}
+
+
 
 object ActorCapabilities extends App {
 
@@ -13,7 +16,7 @@ object ActorCapabilities extends App {
       case SendMessageToYourself(content) =>
         self ! content
       case SayHiTo(ref) => ref ! "Hi!" // alice is being passed as a sender
-      case WirelessPhoneMessage(content, ref) => ref forward (content + "s") // i keep the original sender of the WPM
+      case WirelessPhoneMessage(content, ref) => ref forward (content + "s") // keep the original sender of the WPM
     }
   }
 
@@ -54,4 +57,129 @@ object ActorCapabilities extends App {
   // forwarding = sending a message with the ORIGINAL SENDER
   case class WirelessPhoneMessage(content: String, ref: ActorRef)
   alice ! WirelessPhoneMessage("Hi", bob) // noSender
+
+  /*
+  1. a Counter actor
+    - Increment
+    - Decrement
+    - Print
+   */
+
+  class Counter extends Actor {
+    var count = 0
+
+    override def receive: Receive = {
+      case "Increment" => count += 1
+        println("[counter] received increment command")
+      case "Decrement" => count -= 1
+        println("[counter] received decrement command")
+      case "Print" => println(s"[counter] current count is: $count")
+    }
+  }
+
+  val countActor = system.actorOf(Props[Counter], "CounterActor")
+
+  countActor ! "Decrement"
+  countActor ! "Increment"
+  countActor ! "Increment"
+
+  countActor ! "Print"
+
+  // Implementation with an case object
+
+  // DOMAIN of the counter
+  object Counter {
+    case object Increment
+    case object Decrement
+    case object Print
+  }
+
+  class NewCounter extends Actor {
+    import Counter._
+    var count = 0
+
+    override def receive: Receive = {
+      case Increment => count += 1
+        println("[counter] received increment command")
+      case Decrement => count -= 1
+        println("[counter] received decrement command")
+      case Print => println(s"[counter] current count is: $count")
+    }
+  }
+
+  val newCounter = system.actorOf(Props[NewCounter], "betterCounter")
+
+  (1 to 5).foreach(_ => newCounter ! Counter.Increment)
+  (1 to 2).foreach(_ => newCounter ! Counter.Decrement)
+  newCounter ! Counter.Print
+  /*
+  2. Bank account as an actor
+    receives
+    - Deposit amount
+    - Withdraw amount
+    - Statement
+
+    replies with
+    - Success/Failure
+
+    interact with some other kind of actor
+   */
+
+
+  object BankAccount {
+    case class Deposit(amount: Int)
+    case class Withdraw(amount: Int)
+    case object Statement
+
+    case class TransactionSuccess(message: String)
+    case class TransactionFailure(message: String)
+  }
+
+  class BankAccount extends Actor {
+    import BankAccount._
+    var currentAmount = 0
+
+    override def receive: Receive = {
+      case Deposit(amount) =>
+        if (amount < 0) sender() ! TransactionFailure("invalid deposit number")
+        else {
+          currentAmount += amount
+          sender() ! TransactionSuccess(s"successfully deposited $amount")
+        }
+      case Withdraw(amount) => {
+        if (amount < 0) sender() ! TransactionFailure("invalid withdraw number")
+        else if (currentAmount - amount > 0) {
+          currentAmount -= amount
+          sender() ! TransactionSuccess("successfully withdrew amount")
+        }
+        else sender() ! TransactionFailure("insufficient funds")
+      }
+      case Statement => sender() ! s"Your balance is $currentAmount"
+
+    }
+  }
+
+  object Terminal {
+    case class LiveTheLife(account: ActorRef)
+  }
+  class Terminal extends Actor {
+    import Terminal._
+    import BankAccount._
+
+    override def receive: Receive = {
+      case LiveTheLife(account) =>
+        account ! Deposit(10000)
+        account ! Withdraw(90000)
+        account ! Withdraw(500)
+        account ! Statement
+      case message => println(message.toString)
+    }
+  }
+
+    val account = system.actorOf(Props[BankAccount], "BankAccount")
+    val terminal = system.actorOf(Props[Terminal], "Terminal")
+
+    import Terminal._ // interesting part, does not work without it
+    terminal ! LiveTheLife(account)
+
 }
